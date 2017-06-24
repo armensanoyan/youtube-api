c4b3e9c22b5c7f720736557f9fb933bc3c977f00
ArmenSan <armensanoyan@gmail.com>
2017-06-24 16:47:22 +0400

    style.css

--- public/app.js
+++ public/app.js
@@ -1,29 +1,35 @@
 function init() {
   gapi.client.setApiKey("AIzaSyD0mQQQdzI4o2DjRhcv2gpAhFDva4Z4uNU");
   gapi.client.load("youtube","v3", () => {
+    console.log(gapi.client.youtube)
   })
 }
 
 (function () {
   document.getElementById('submit').addEventListener('click', function(e) {
     e.preventDefault();
+    const results = document.getElementById('results');
+    
+    // while(results.hasChildNodes) results.removeChild(results.lastChild)
 
     var request = gapi.client.youtube.search.list({
          part: "snippet",
          type: "video",
-         q: encodeURIComponent(document.getElementById("search").value).replace(/%20/g, "+"),
-         maxResults: 3,
-         order: "viewCount",
-         publishedAfter: "2015-01-01T00:00:00Z"
+         q: document.getElementById("search").value,
+        //  q: encodeURIComponent(document.getElementById("search").value).replace(/%20/g, "+"),
+         maxResults: 5,
+         order: "relevance",
+        //  publishedAfter: "2015-01-01T00:00:00Z"
     }); 
 
     request.execute(response => {
+      console.log(JSON.stringify(response.result))
       response.result.items.forEach(function(element) {
-        const results = document.getElementById('results')
         const hOne = document.createElement('h3');
         hOne.textContent = element.snippet.title;
         const iframe =document.createElement('iframe')
         iframe.src="//www.youtube.com/embed/" + element.id.videoId;
+        console.log(iframe.src)
         const box = document.createElement('div');
         box.appendChild(hOne);
         box.appendChild(iframe);
